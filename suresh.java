c3d0510864c660e1a72da83f97bbdecf1663e108
sureshsreddy16 <57609555+sureshsreddy16@users.noreply.github.com>
2019-11-12 09:07:24 +0530

    this is going to commit

---
+++ suresh.java
@@ -0,0 +1,10 @@
+
+/** 
+ * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
+ */
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, besant");
+System.out.println("flipkart, besant");
+  }
+}
